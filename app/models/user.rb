edc821ed58c617102ff03379e284b878f9d5196f
daichi hagiwara <daichi.hagiwara@livesense.co.jp>
2018-02-05 11:19:25 +0000

    userモデルにmicropostとの多対多の関係を表すためにhas_manyメソッドを記載する。

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,9 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
-
+  has_many :favorites
+  has_many :fav_microposts, through: :favorites, source: :micropost
+  
   def follow(other_user)
     unless self == other_user
       self.relationships.find_or_create_by(follow_id: other_user.id)
@@ -30,4 +32,5 @@ class User < ApplicationRecord
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
   end
+    
 end
